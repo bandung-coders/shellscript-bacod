01ba57c1bb3b33f1c168665451041610441c0e5e
Restu Dwi Cahyo <dwi.restu3@gmail.com>
2022-09-03 01:30:16 +0700

    Done automate all repo for roofotop Project

--- rooftopcoffee/ServerMenu.sh
+++ rooftopcoffee/ServerMenu.sh
@@ -68,16 +68,13 @@ ${YELLOW}Choose an option:  ${CYAN}"
     read -r ans
     case $ans in
     1)
-        sub-submenu
-        backendMenu
+        beOfficeApi
         ;;
     2)
-        menu
-        backendMenu
+        bePublicApi
         ;;
     3)
-        menu
-        backendMenu
+        beSocket
         ;;
     99)
         mainmenu
@@ -116,20 +113,16 @@ ${YELLOW}Choose an option:  ${CYAN}"
     read -r ans
     case $ans in
     1)
-        sub-submenu
-        frontendMenu
+        feDashboard
         ;;
     2)
-        menu
-        frontendMenu
+        feKitchen
         ;;
     3)
-        menu
-        frontendMenu
+        feCashier
         ;;
     4)
-        menu
-        frontendMenu
+        feMenu
         ;;
     99)
         mainmenu
@@ -143,6 +136,149 @@ ${YELLOW}Choose an option:  ${CYAN}"
         exit 1
         ;;
     esac
+}
+
+
+beOfficeApi() {
+    echo "${CYAN}Deploying ( Backend - Office Api )"
+
+    source /home/dieunika/nodevenv/sampaimasukangin/backend/office-api/14/bin/activate && cd /home/dieunika/sampaimasukangin/backend/office-api
+
+    git pull origin production
+
+    npx npm install
+
+    npx pm2 list
+
+    npx pm2 restart backend-office-api
+
+    npx pm2 save --force
+
+    npx pm2 list
+}
+
+bePublicApi() {
+    echo "${CYAN}Deploying ( Backend - Public Api )"
+
+    source /home/dieunika/nodevenv/sampaimasukangin/backend/public-api/14/bin/activate && cd /home/dieunika/sampaimasukangin/backend/public-api
+
+    git pull origin production
+
+    npx npm install
+
+    npx pm2 list
+
+    npx pm2 restart backend-public-api
+
+    npx pm2 save --force
+
+    npx pm2 list
+}
+
+beSocket() {
+    echo "${CYAN}Deploying ( Backend - Socket )"
+
+    source /home/dieunika/nodevenv/sampaimasukangin/backend/socket/14/bin/activate && cd /home/dieunika/sampaimasukangin/backend/socket
+
+    git pull origin production
+
+    npx npm install
+
+    npx pm2 list
+
+    npx pm2 restart backend-socket
+
+    npx pm2 save --force
+
+    npx pm2 list
+}
+
+feDashboard() {
+    echo "${CYAN}Deploying ( Frontend - Dashboard )"
+
+    source /home/dieunika/nodevenv/sampaimasukangin/frontend/dashboard/14/bin/activate && cd /home/dieunika/sampaimasukangin/frontend/dashboard
+
+    git pull origin production
+
+    npx npm install
+
+    npx pm2 list
+
+    npx npm run build
+
+    npx pm2 restart frontend-dashboard
+
+    npx pm2 save
+
+    npx pm2 save --force
+
+    npx pm2 list
+}
+
+feCashier() {
+    echo "${CYAN}Deploying ( Frontend - Cashier )"
+
+    source /home/dieunika/nodevenv/sampaimasukangin/frontend/cashier/14/bin/activate && cd /home/dieunika/sampaimasukangin/frontend/cashier
+
+    git pull origin production
+
+    npx npm install
+
+    npx pm2 list
+
+    npx npm run build
+
+    npx pm2 restart frontend-cashier
+
+    npx pm2 save
+
+    npx pm2 save --force
+
+    npx pm2 list
+}
+
+feKitchen() {
+    echo "${CYAN}Deploying ( Frontend - Kitchen )"
+
+    source /home/dieunika/nodevenv/sampaimasukangin/frontend/kitchen/14/bin/activate && cd /home/dieunika/sampaimasukangin/frontend/kitchen
+
+    git pull origin production
+
+    npx npm install
+
+    npx pm2 list
+
+    npx npm run build
+
+    npx pm2 restart frontend-kitchen
+
+    npx pm2 save
+
+    npx pm2 save --force
+
+    npx pm2 list
+}
+
+feMenu() {
+    echo "${CYAN}Deploying ( Frontend - Menu )"
+
+    source /home/dieunika/nodevenv/sampaimasukangin/frontend/menu/14/bin/activate && cd /home/dieunika/sampaimasukangin/frontend/menu
+
+    git pull origin production
+
+    npx npm install
+
+    npx pm2 list
+
+    npx npm run build
+
+    npx pm2 restart frontend-menu
+
+    npx pm2 save
+
+    npx pm2 save --force
+
+    npx pm2 list
 }
 
 mainmenu
